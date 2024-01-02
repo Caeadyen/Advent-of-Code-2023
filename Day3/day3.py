bdf8c44805f6087fac86085b0afc3dea9f36dfc4
hrings <hrings@student.42.fr>
2024-01-02 21:19:58 +0100

    Day3 part 2 done

--- Day3/day3.py
+++ Day3/day3.py
@@ -3,11 +3,11 @@ file = open('input.txt')
 temp = file.read().splitlines()
 
 test = list(temp)
-number = 0
-toAdd = False
+
+
 maxRow = len(test) - 1
 maxCol = len(test[0]) -1
-result = 0
+
 
 def in_bounds(row,col):
 	if row < 0 or col < 0:
@@ -20,23 +20,122 @@ def findSymbol(row,col):
 	for i in range(-1, 2):
 		for j in range(-1, 2):
 			if(in_bounds(row + i,col + j)):
-				#print('Symbol: ', test[row + i][col + j])
-				#print('If: ', not test[row + i][col + j].isnumeric() and not test[row + i][col + j] == '.')
 				if(not test[row + i][col + j].isnumeric() and not test[row + i][col + j] == '.'):
 					return True
 	return False
 
-for i in range(len(test)):
-	for j in range(len(test[i])):
-		if(test[i][j].isnumeric()):
-			number = number * 10 + int(test[i][j])
-			if(findSymbol(i,j)):
-				toAdd = True
+def findNumber(row,col):
+	i = -1
+	j = -1
+	number = 0
+	result = 0
+	count = 0
+
+	while j < 2:
+		if(in_bounds(row + i,col + j)):
+			if(test[row + i][col + j].isnumeric()):
+				number = 0
+				while (in_bounds(row + i,col + j) and test[row + i][col + j].isnumeric()):
+					j -= 1
+				j += 1
+				while (in_bounds(row + i,col + j) and test[row + i][col + j].isnumeric()):
+					number = number * 10 + int(test[row + i][col + j])
+					j += 1
+				if(result):
+					result *= number
+					count += 1
+				else:
+					result = number
+					count += 1
+			else:
+				j += 1
 		else:
-			if(number and toAdd):
-				result +=  number
-			toAdd = False
-			number = 0
+			j += 1
 
+	i = 0
+	j = -1
+	if(in_bounds(row + i,col + j)):
+			if(test[row + i][col + j].isnumeric()):
+				number = 0
+				while (in_bounds(row + i,col + j) and test[row + i][col + j].isnumeric()):
+					j -= 1
+				j += 1
+				while (in_bounds(row + i,col + j) and test[row + i][col + j].isnumeric()):
+					number = number * 10 + int(test[row + i][col + j])
+					j += 1
+				if(result):
+					result *= number
+					count += 1
+				else:
+					result = number
+					count += 1
+			else:
+				j += 1
+	j = 1
+	if(in_bounds(row + i,col + j)):
+			if(test[row + i][col + j].isnumeric()):
+				number = 0
 
-print(result)
+				while (in_bounds(row + i,col + j) and test[row + i][col + j].isnumeric()):
+					number = number * 10 + int(test[row + i][col + j])
+					j += 1
+				if(result):
+					result *= number
+					count += 1
+				else:
+					result = number
+					count += 1
+			else:
+				j += 1
+	i = 1
+	j = -1
+	while j < 2:
+		if(in_bounds(row + i,col + j)):
+			if(test[row + i][col + j].isnumeric()):
+				number = 0
+				while (in_bounds(row + i,col + j) and test[row + i][col + j].isnumeric()):
+					j -= 1
+				j += 1
+				while (in_bounds(row + i,col + j) and test[row + i][col + j].isnumeric()):
+					number = number * 10 + int(test[row + i][col + j])
+					j += 1
+				if(result):
+					result *= number
+					count += 1
+				else:
+					result = number
+					count += 1
+			else:
+				j += 1
+		else:
+			j += 1
+	if count == 2:
+		return result
+	return 0
+
+def part_one():
+	number = 0
+	result = 0
+	toAdd = False
+	for i in range(len(test)):
+		for j in range(len(test[i])):
+			if(test[i][j].isnumeric()):
+				number = number * 10 + int(test[i][j])
+				if(findSymbol(i,j)):
+					toAdd = True
+			else:
+				if(number and toAdd):
+					result +=  number
+				toAdd = False
+				number = 0
+	print('Part one: ', result)
+
+def part_two():
+	result = 0
+	for i in range(len(test)):
+		for j in range(len(test[i])):
+			if(test[i][j]== '*'):
+				result += findNumber(i,j)
+	print('Part two: ', result)
+part_one()
+part_two()
